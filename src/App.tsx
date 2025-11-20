17f11a62fdda8d83c1dd35aa1cefcd63567cfc92
Jefferson Lima <jefferson.fl123@gmail.com>
2025-11-20 19:13:18 -0300

    chore: setup mui and first theme definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
+import { CssBaseline, ThemeProvider, Typography } from '@mui/material'
 import './App.css'
+import { theme } from './providers/theme/theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://pokeapi.co/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gotta Catch'Em All!!!!
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme} defaultMode="dark">
+        <Typography variant="h4">Hello, World!</Typography>
+        <CssBaseline />
+      </ThemeProvider>
+    </>
   )
 }
 
---
+++ src/providers/theme/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles'
+import {} from '@mui/material/styles'
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: 'Outfit, sans-serif',
+  },
+
+  palette: {
+    background: {
+      default: '#0A141E',
+    },
+    text: {
+      primary: '#FFFFFF',
+    },
+    divider: '#64C8FF',
+  },
+})
