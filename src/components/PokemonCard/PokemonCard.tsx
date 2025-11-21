2c32d12bc365e85b78a5580b0a814ba45d28ef0f
Jefferson Lima <jefferson.fl123@gmail.com>
2025-11-20 21:38:08 -0300

    feat: create PokemonCard component

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,65 @@
+import { Box, Card, CardMedia, Divider, Typography } from '@mui/material'
+import { Pokemon } from '../../types/Pokemon'
+import { Chip } from '../ui/Chip/Chip'
+
+type PokemonCardProps = {
+  pokemon: Pokemon
+}
+
+export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
+  return (
+    <Card
+      sx={{
+        padding: 2,
+        textAlign: 'center',
+        width: 180,
+        height: 260,
+        backgroundColor: 'transparent',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        boxShadow: '0 0 6px rgba(59,140,255,0.45)',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        alignItems: 'center',
+        borderRadius: 4,
+      }}
+    >
+      <Box
+        sx={{
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          borderWidth: 1,
+          borderStyle: 'solid',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <CardMedia
+          component="image"
+          image={pokemon.other['official-artwork'].front_default || ''}
+          sx={{
+            width: '80px',
+            height: '80px',
+          }}
+        />
+      </Box>
+      <Divider sx={{ width: '100%' }} />
+      <Typography variant="h5" component="h2" textTransform="capitalize">
+        {pokemon.name}
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: 1,
+        }}
+      >
+        {pokemon.types.map((typeInfo) => (
+          <Chip key={typeInfo.type.name} label={typeInfo.type.name} />
+        ))}
+      </Box>
+    </Card>
+  )
+}
---
+++ src/types/Pokemon.tsx
@@ -0,0 +1,45 @@
+export type StatName =
+  | 'hp'
+  | 'attack'
+  | 'defense'
+  | 'special-attack'
+  | 'special-defense'
+  | 'speed'
+
+export interface PokemonStat {
+  base_stat: number
+  effort: number
+  stat: {
+    name: StatName
+    url: string
+  }
+}
+
+export interface PokemonType {
+  slot: number
+  type: { name: string; url: string }
+}
+
+export interface PokemonSprites {
+  front_default?: string | null
+  other?: { 'official-artwork'?: { front_default?: string | null } }
+}
+
+export interface Pokemon {
+  id: number
+  name: string
+  base_experience?: number
+  height?: number
+  weight?: number
+  types: PokemonType[]
+  abilities?: {
+    ability: { name: string; url: string }
+    is_hidden: boolean
+    slot: number
+  }[]
+  stats: PokemonStat[]
+  sprites?: PokemonSprites
+  other: {
+    'official-artwork': { front_default?: string | null }
+  }
+}
