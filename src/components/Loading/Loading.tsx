05db5f8015abee20490b96ae5661e572c28b6185
Jefferson Lima <jefferson.fl123@gmail.com>
2025-11-23 13:19:46 -0300

    style: format Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -25,7 +25,7 @@ export const Loading = () => {
   return (
     <Box>
       <Svg
-      data-testid="loading-svg"
+        data-testid="loading-svg"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="-10.415 -10.415 250 250"
         fill="none"
@@ -37,19 +37,23 @@ export const Loading = () => {
         width="250"
       >
         <desc>Pokeball Streamline Icon: https://streamlinehq.com</desc>
-        <path data-testid="loading-path"
+        <path
+          data-testid="loading-path"
           d="M28.646250000000002 114.58500000000001a85.93875 85.93875 0 1 0 171.8775 0 85.93875 85.93875 0 1 0 -171.8775 0"
           stroke-width="20.83"
         ></path>
-        <path data-testid="loading-path"
+        <path
+          data-testid="loading-path"
           d="M85.93875 114.58500000000001a28.646250000000002 28.646250000000002 0 1 0 57.292500000000004 0 28.646250000000002 28.646250000000002 0 1 0 -57.292500000000004 0"
           stroke-width="20.83"
         ></path>
-        <path data-testid="loading-path"
+        <path
+          data-testid="loading-path"
           d="M28.646250000000002 114.58500000000001h57.292500000000004"
           stroke-width="20.83"
         ></path>
-        <path data-testid="loading-path"
+        <path
+          data-testid="loading-path"
           d="M143.23125 114.58500000000001h57.292500000000004"
           stroke-width="20.83"
         ></path>
